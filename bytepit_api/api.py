1ff5685f677aa1785dd07d602d993fb72296c482
fran <fran.sipic@sedamit.hr>
2023-11-07 18:21:46 +0100

    created login form model, refactoring

--- bytepit_api/api.py
+++ bytepit_api/api.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, FastAPI
 
-from bytepit_api.routers.auth import router as auth_router
 from bytepit_api.routers.admin import router as admin_router
+from bytepit_api.routers.auth import router as auth_router
 
 
 router = APIRouter()
-router.include_router(auth_router)
 router.include_router(admin_router)
+router.include_router(auth_router)
 
 app = FastAPI()
 app.include_router(router)
