f266b44c2549bdc047986deffae1b4cbb8cc5c87
Jakov Vinožganić <yourgitemail@example.com>
2023-11-09 21:47:05 +0100

    Login and required dependencies

--- bytepit_api/database/queries.py
+++ bytepit_api/database/queries.py
@@ -1,11 +1,18 @@
-import uuid
-
 from bytepit_api.database import db
 from bytepit_api.models.auth_schemes import UserInDB
 
 
 def get_user_by_username(username: str):
     query_tuple = ("SELECT * FROM users WHERE username = %s", (username,))
+    result = db.execute_one(query_tuple)
+    if result["result"]:
+        return UserInDB(**result["result"][0])
+    else:
+        return None
+
+
+def get_user_by_email(email: str):
+    query_tuple = ("SELECT * FROM users WHERE email = %s", (email,))
     result = db.execute_one(query_tuple)
     if result["result"]:
         return UserInDB(**result["result"][0])
--- bytepit_api/helpers/login_helpers.py
+++ bytepit_api/helpers/login_helpers.py
@@ -1,12 +1,12 @@
 import os
-from datetime import datetime, timedelta, timezone
 
+from datetime import datetime, timedelta, timezone
 from typing import Union
 
-from jose import JWTError, jwt
+from jose import jwt
 from passlib.context import CryptContext
 
-from bytepit_api.database.queries import get_user_by_username
+from bytepit_api.database.queries import get_user_by_email, get_user_by_username
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALGORITHM = "HS256"
@@ -19,8 +19,12 @@ def verify_password(plain_password, password_hash):
     return pwd_context.verify(plain_password, password_hash)
 
 
-def authenticate_user(username: str, password: str):
-    user = get_user_by_username(username)
+def check_if_email(identifier: str) -> bool:
+    return "@" in identifier
+
+
+def authenticate_user(identifier: str, password: str):
+    user = get_user_by_email_or_username(identifier)
     if not user:
         return False
     if not verify_password(password, user.password_hash):
@@ -37,3 +41,9 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_user_by_email_or_username(identifier: str):
+    is_email = check_if_email(identifier)
+    user = get_user_by_email(identifier) if is_email else get_user_by_username(identifier)
+    return user
