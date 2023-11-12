9ca12e4e1253313a2dccd3a084d484b40619d172
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-11-13 00:28:38 +0100

    Change code regarding comments

--- bytepit_api/routers/admin.py
+++ bytepit_api/routers/admin.py
@@ -1,37 +1,31 @@
 import uuid
 
-from typing import Annotated
+from typing import Annotated, List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from bytepit_api.dependencies.auth_dependencies import get_current_admin_user
-from bytepit_api.models.auth_schemes import Role, User, UserInDB, Users, UsersInDB
+from bytepit_api.models.auth_schemes import Role, UserInDB
 from bytepit_api.database.queries import get_users, get_unverified_organisers
 
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 
-@router.get("/list-users")
-def list_users() -> list[UserInDB]:
-    users = get_users()
-    if users is None:
-        raise HTTPException(status_code=500, detail="Internal server error")
-    if len(users.users) == 0:
-        raise HTTPException(status_code=404, detail="No users found")
-    return users.users
+@router.get(
+    "/list-users",
+    response_model=List[UserInDB],
+)
+def list_users():
+    return get_users()
 
 
 @router.get(
     "/unverified-organisers",
+    response_model=List[UserInDB],
 )
-def list_unverified_organisers() -> list[UserInDB]:
-    organizers = get_unverified_organisers()
-    if organizers is None:
-        raise HTTPException(status_code=500, detail="Internal server error")
-    if len(organizers.users) == 0:
-        raise HTTPException(status_code=404, detail="No unverified organisers found")
-    return organizers.users
+def list_unverified_organisers():
+    return get_unverified_organisers()
 
 
 @router.post("/confirm-organiser/{user_id}")
