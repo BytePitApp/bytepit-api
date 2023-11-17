db683c7eb928047637c1c8d07addd15b7245e71f
Marko Bolt <marko.bolt@fer.hr>
2023-11-17 12:47:34 +0100

    added image to registration

--- bytepit_api/database/queries.py
+++ bytepit_api/database/queries.py
@@ -32,15 +32,15 @@ def get_user_by_id(user_id: uuid.UUID):
         return None
 
 
-def create_user(username, password_hash, name, surname, email, role, confirmation_token):
+def create_user(username, password_hash, name, surname, email, role, image, confirmation_token):
     approved_by_admin = False if role == "organiser" else True
 
     user_insert_query = (
         """
-        INSERT INTO users (username, password_hash, name, surname, email, role, is_verified, approved_by_admin)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO users (username, password_hash, name, surname, email, role, image, is_verified, approved_by_admin)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """,
-        (username, password_hash, name, surname, email, role, False, approved_by_admin),
+        (username, password_hash, name, surname, email, role, image.file.read(), False, approved_by_admin),
     )
 
     token_insert_query = (
--- bytepit_api/helpers/register_helpers.py
+++ bytepit_api/helpers/register_helpers.py
@@ -13,10 +13,10 @@ def generate_confirmation_token():
     return secrets.token_urlsafe(20)
 
 
-def register_user(username, password, name, surname, email, role):
+def register_user(username, password, name, surname, email, role, image):
     confirmation_token = generate_confirmation_token()
     password_hash = get_password_hash(password)
-    result = create_user(username, password_hash, name, surname, email, role, confirmation_token)
+    result = create_user(username, password_hash, name, surname, email, role, image, confirmation_token)
     if not result:
         return None
     return confirmation_token
