d4f608c6c994898ec3300942071eb3984c94dc1f
Marko Bolt <marko.bolt@fer.hr>
2024-01-19 18:56:19 +0100

    get user by username

--- bytepit_api/services/auth_service.py
+++ bytepit_api/services/auth_service.py
@@ -92,8 +92,8 @@ def confirm_email(verification_token: str):
     return {"message": "User activated"}
 
 
-def get_user(id: uuid.UUID):
-    user = auth_queries.get_user_by_id(id)
+def get_user(username: str):
+    user = auth_queries.get_user_by_username(username)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found")
     return user
